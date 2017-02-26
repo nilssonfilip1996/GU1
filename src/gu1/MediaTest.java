c762169eac56179604eee4266e74afa4bc0bfaa2
Silvester Dezsi <silvester@dezsi.se>
2017-02-26 18:52:15 +0100

    push

--- src/gu1/MediaTest.java
+++ src/gu1/MediaTest.java
@@ -9,38 +9,34 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+public abstract class MediaTest<E> implements List<E> {
 
-	public abstract class MediaTest<E> implements List<E> {
+	public static void main(String[] args) throws IOException {
 
-		public static void main(String[] args) throws IOException {
+		File file = new File("GU1/src/gu1/Media.txt");
+		Scanner s = new Scanner(file);
+		ArrayList<String> list = new ArrayList<String>();
 
-			File file = new File("Media.txt");
-			Scanner s = new Scanner(new File("Media.txt"));		
-			ArrayList<String> list = new ArrayList<String>();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
 
-			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-			
-			while (s.hasNextLine())
-				list.add(s.nextLine());
-				s.close();
+		while (s.hasNextLine())
+			list.add(s.nextLine());
+		s.close();
 
-				
-				for(String x : list){
-					System.out.println(x);
-				}
-
-				
-			String str;
-			String[] values;
-			Media media;
-			 while((str = reader.readLine() ) !=null){
-			 values = str.split(";");
-//			 lantagare = new Lantagare( values[ 0 ], values[ 1 ], values[ 2 ] );
-//			 map.put( values[ 0 ], lantagare );
-			
-			 System.out.println(str);
+		for (String x : list) {
+			System.out.println(x);
 		}
 
+		String str;
+		String[] values;
+		Media media;
+		while ((str = reader.readLine()) != null) {
+			values = str.split(";");
+			//lantagare = new Lantagare( values[ 0 ], values[ 1 ], values[ 2 ] );
+			//map.put( values[ 0 ], lantagare );
+
+			System.out.println(str);
+		}
 	}
-	}
+}
 

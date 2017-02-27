36c7cce0fe5058c2a92528b6fa5109e0a1075f7a
Cortez <Cortez@jespers-mbp-2.clients.internal.mah.se>
2017-02-27 12:11:31 +0100

    Lite hashTest...smooooke!

---
+++ src/gu1/HashCode.java
@@ -0,0 +1,59 @@
+package gu1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JOptionPane;
+
+public class HashCode {
+
+	public static void main(String[] args) throws IOException {
+
+		Path path = Paths.get(System.getProperty("user.dir")).resolve("lantagare.txt");
+		BufferedReader reader = new BufferedReader(new FileReader(path.toFile()));
+
+		Object Input = (JOptionPane.showInputDialog(null, "Skriv in ett namn eller personnummer"));
+
+		Map<String, Integer> nameAndNumber = new HashMap<>();
+		String line = reader.readLine();
+		while (line != null) {
+			System.out.println("Processeserade rader: " + line);
+			if (!line.trim().equals(" ")) {
+				String[] parts = line.split(";");
+
+				for (String info : parts) {
+					if (info == null || info.trim().equals("")) {
+						continue;
+					}
+					Integer value = 0;
+
+					if (info.equals(Input)) {
+						nameAndNumber.put(info, 1);
+
+					} else {
+						nameAndNumber.put(info, value);
+
+					}
+
+				}
+			}
+
+			line = reader.readLine();
+		}
+		System.out.println(nameAndNumber);
+
+		for (String info : nameAndNumber.keySet()) {
+			if (nameAndNumber.get(Input) == 1) {
+				JOptionPane.showConfirmDialog(null, "Namnet finns");
+				break;
+			} else {
+
+			}
+		}
+	}
+}

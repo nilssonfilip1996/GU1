f0402afc3572de63f8de3e8602e3f907ecb2e422
Silvester Dezsi <silvester@dezsi.se>
2017-03-04 01:11:58 +0100

    ...

--- src/gu1/Controller.java
+++ src/gu1/Controller.java
@@ -7,68 +7,68 @@ public class Controller {
 	LinkedList<Media> mediaList;
 	LinkedList<User> userList;
 
-	public Controller(String mediaListPath, String userListPath) throws IOException {
-		populateUserList(userListPath);
-		populateMediaList(mediaListPath);
-		
-		//make loans and print user
+	public Controller(String mediaListPath, String userListPath) {
+		this.userList = populateUserList(userListPath);
+		this.mediaList = populateMediaList(mediaListPath);
+
+		// make loans and print user
 		this.userList.get(1).newLoan(this.mediaList.get(1));
 		this.userList.get(1).newLoan(this.mediaList.get(2));
 		this.userList.get(1).newLoan(this.mediaList.get(3));
 		System.out.println(this.userList.get(1).toString());
-		
-		//print userlist
-		for(int i=0; i<userList.size(); i++)
+
+		// print userlist
+		for (int i = 0; i < userList.size(); i++)
 			System.out.println(userList.get(i).toString());
-		
-		
-		//print medialist
-		for(int i=0; i<mediaList.size(); i++)
+
+		// print medialist
+		for (int i = 0; i < mediaList.size(); i++)
 			System.out.println(mediaList.get(i).toString());
 	}
 
-	public void populateUserList(String userListPath) throws IOException {
+	public LinkedList<User> populateUserList(String userListPath) {
 		LinkedList<User> userList = new LinkedList<User>();
-		FileInputStream fis = new FileInputStream(userListPath);
-		InputStreamReader isr = new InputStreamReader(fis, "ISO-8859-1");
-		BufferedReader reader = new BufferedReader(isr);
+		try {
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(new FileInputStream(userListPath), "ISO-8859-1"));
 
-		String str;
-		String[] values;
-		User user;
-		while ((str = reader.readLine()) != null) {
-			values = str.split(";");
-			user = new User(values[0], values[1], values[2]);
-			userList.add(user);
+			String str;
+			String[] values;
+			while ((str = reader.readLine()) != null) {
+				values = str.split(";");
+				userList.add(new User(values[0], values[1], values[2]));
+			}
+			reader.close();
+		} catch (IOException e) {
+			System.out.println(e);
 		}
-		reader.close();
-		this.userList = userList;
+		return userList;
 	}
 
-	public void populateMediaList(String mediaListPath) throws IOException {
+	public LinkedList<Media> populateMediaList(String mediaListPath) {
 		LinkedList<Media> mediaList = new LinkedList<Media>();
-		FileInputStream fis = new FileInputStream(mediaListPath);
-		InputStreamReader isr = new InputStreamReader(fis, "ISO-8859-1");
-		BufferedReader reader = new BufferedReader(isr);
+		try {
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(new FileInputStream(mediaListPath), "ISO-8859-1"));
 
-		String str;
-		String[] values;
-		Media media;
-		while ((str = reader.readLine()) != null) {
-			values = str.split(";");
-			if (values[0].equals("Dvd")) {
-				String[] actors = new String[values.length - 4];
-				for (int i = 0; i < actors.length; i++)
-					actors[i] = values[i + 4];
-				media = new Dvd(values[1], values[2], values[3], actors);
-				mediaList.add(media);
-			} else if (values[0].equals("Bok")) {
-				media = new Book(values[1], values[2], values[3], values[4]);
-				mediaList.add(media);
+			String str;
+			String[] values;
+			while ((str = reader.readLine()) != null) {
+				values = str.split(";");
+				if (values[0].equals("Dvd")) {
+					String[] actors = new String[values.length - 4];
+					for (int i = 0; i < actors.length; i++)
+						actors[i] = values[i + 4];
+					mediaList.add(new Dvd(values[1], values[2], values[3], actors));
+				} else if (values[0].equals("Bok")) {
+					mediaList.add(new Book(values[1], values[2], values[3], values[4]));
+				}
 			}
+			reader.close();
+		} catch (IOException e) {
+			System.out.println(e);
 		}
-		reader.close();
-		this.mediaList = mediaList;
+		return mediaList;
 	}
 
 }
--- src/gu1/MainLauncher.java
+++ src/gu1/MainLauncher.java
@@ -1,6 +1,5 @@
 package gu1;
 
-import java.io.IOException;
 import javax.swing.SwingUtilities;
 
 public class MainLauncher {
@@ -8,11 +7,7 @@ public class MainLauncher {
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				try {
-					Controller controller = new Controller("Media.txt", "Lantagare.txt");
-				} catch (IOException e) {
-					System.out.print(e);
-				}
+				Controller controller = new Controller("Media.txt", "Lantagare.txt");
 			}
 		});
 	}

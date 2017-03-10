39e6c980d2ef85fc5155a25fe5f5ba3c6a637ced
nilssonfilip1996 <nilssonfilip1996@hotmail.com>
2017-03-10 14:51:06 +0100

    Börjar pilla lite med GUI:t från scratch...

---
+++ src/quarantine/GuiTest2.java
@@ -0,0 +1,31 @@
+package quarantine;
+
+import java.awt.event.*;
+
+import javax.swing.*;
+
+import gu1.*;
+
+public class GuiTest2 extends JFrame{
+	
+	
+	public GuiTest2(Controller controller){
+		
+	}
+	
+	
+	
+
+
+	private class ButtonListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			
+		}
+
+	}
+}
+
+

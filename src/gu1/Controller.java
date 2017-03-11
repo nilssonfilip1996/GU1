7f8d6bd8a82cb014305652add0edb8feb7b037f9
Filip Nilsson <Filip Nilsson@PureGaming>
2017-03-11 11:45:50 +0100

    d

--- src/gu1/Controller.java
+++ src/gu1/Controller.java
@@ -174,7 +174,7 @@ public class Controller {
 	public boolean borrowMedia(String mediaID) {
 		if (mediaLibrary.contains(mediaID)) {
 			currentUser.borrowMedia(mediaLibrary.borrowMedia(mediaID));
-			System.err.println(("User: " + currentUser.getName() + " has " +currentUser.loans().size() + " loans"));
+			System.err.println(("User: " + currentUser.getName() + " has " +currentUser.loans().size() + " loans"));	//test för att se antal lånade ex.
 			mainWindow.updateMediaLists(populateAvailableMediaList2(), populateCurrentUserLoanList2());
 //			test.addLoan(mediaLibrary.get(mediaID).getTitel()); // <-------
 //																// testing

79aa95e39900d6b719199777d6071fd9d64d35bc
nilssonfilip1996 <nilssonfilip1996@hotmail.com>
2017-03-15 16:37:25 +0100

    Rapporten återställd!

--- src/gu1/MediaViewer.java
+++ src/gu1/MediaViewer.java
@@ -4,14 +4,15 @@ import java.awt.*;
 
 import javax.swing.*;
 
-
 /**
- * The class MediaViewer represents the graphical interface of the Library program.
- * The class uses two instances of JPanel(LogInPanel & LibraryPanel) to display different kinds of content.
- * A Controller instance decides which of these two panels to show.
+ * The class MediaViewer represents the graphical interface of the Library
+ * program. The class uses two instances of JPanel(LogInPanel & LibraryPanel) to
+ * display different kinds of content. A Controller instance decides which of
+ * these two panels to show.
  * 
  * 
- * @author Jesper Anderberg, Filip Nilsson, Aron Polner, Ali Hassan, Szilveszter Dezsi
+ * @author Jesper Anderberg, Filip Nilsson, Aron Polner, Ali Hassan, Szilveszter
+ *         Dezsi
  *
  */
 public class MediaViewer extends JFrame {
@@ -20,7 +21,9 @@ public class MediaViewer extends JFrame {
 	private LibraryPanel libraryPanel;
 
 	/**
-	 * The constructor draws the frame together with an instance of LogInPanel and an instance of LibraryPanel. 
+	 * The constructor draws the frame together with adding an instance of
+	 * LogInPanel and an instance of LibraryPanel. When constructed, only the
+	 * LogInPanel is set to visible and not the LibraryPanel.
 	 * 
 	 * @param controller
 	 */
@@ -45,27 +48,56 @@ public class MediaViewer extends JFrame {
 		libraryPanel.setVisible(false);
 	}
 
+	/**
+	 * Calls for the method login in the class controller.
+	 * 
+	 * @param idEntered
+	 */
 	public void logInPressed(String idEntered) {
 		controller.login(idEntered);
 	}
 
+	/**
+	 * Sets the LoginPanel to invisible and the LibraryPanel to visible. Also
+	 * passes a userName to the LibraryPanel.
+	 * 
+	 * @param name
+	 */
 	public void loginToLibraryPanel(String name) {
 		logInPanel.setVisible(false);
 		libraryPanel.fillUserName(name);
 		libraryPanel.setVisible(true);
 	}
-	
-	public void updateMediaLists(Media[] availableMediaList, Media[] userLoanList){
+
+	/**
+	 * Used to display the available media and the loans of a specific user.
+	 * 
+	 * @param availableMediaList
+	 *            , media-array to display as available.
+	 * @param userLoanList
+	 *            , media-array to show the logged in users loans.
+	 */
+	public void updateMediaLists(Media[] availableMediaList, Media[] userLoanList) {
 		libraryPanel.fillAvailableMediaList(availableMediaList);
 		libraryPanel.fillUserLoanList(userLoanList);
 	}
 
+	/**
+	 * Recieves the info of a specific media as a String and passes it to the
+	 * method showMediaInfo in the class LibraryPanel.
+	 * 
+	 * @param media
+	 *            , information of a certain media within the library.
+	 */
 	public void updateMediaInfoField(String media) {
-		libraryPanel.showMediaInfo(media);	
+		libraryPanel.showMediaInfo(media);
 	}
 
+	/**
+	 * Sets the logInPanel to visible and the libraryPanel to invisible.
+	 */
 	public void libraryToLoginPanel() {
 		logInPanel.setVisible(true);
-		libraryPanel.setVisible(false);	
+		libraryPanel.setVisible(false);
 	}
 }

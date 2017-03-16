f7f7ed7f34be4d59ec58c56e78cb6e589e1ccfb8
Silvester Dezsi <silvester@dezsi.se>
2017-03-16 15:35:58 +0100

    User och UserDatabase

--- src/gu1/User.java
+++ src/gu1/User.java
@@ -1,56 +1,105 @@
 package gu1;
 
 import java.util.LinkedList;
-
+/**
+ * A class that holds the the infomation of a library borrower such a
+ * name, Id, phone number and the loans of a boroower.  
+ * @author Jesper Anderberg, Filip Nilsson, Aron Polner, Ali Hassan, Szilveszter Dezsi
+ *
+ */
 public class User {
 	private String id;
 	private String name;
 	private String phoneNr;
 	private LinkedList<Media> loans;
 	
+	/**
+	 * A constructor that takes three arguments
+	 *the Id , name and the phone number of the library borrower.
+	 * constructor initietes the loans of the borrower.
+	 * @param id
+	 * @param name
+	 * @param phoneNr
+	 * @param loans
+	 */
 	public User(String id, String name, String phoneNr) {
 		this.id = id;
 		this.name = name;
 		this.phoneNr = phoneNr;
 		this.loans = new LinkedList<Media>();
 	}
-	
+	/**
+	 * A method that returns the media object 
+	 * and removes the object from the borrowers loan list.
+	 * @return the index of the removed media.
+	 */
 	public Media returnMedia(Media media){
 		return loans.remove(loans.indexOf(media));
 	}
-	
+	/**
+	 * A method that allows the borrower to borrow a media object 
+	 * and adds the object to the borrowers loan list .
+	 * @param media the object to be borrowed.
+	 */
 	public void borrowMedia(Media media){
 		loans.add(media);
 	}
-	
+	/**
+	 * A method that returns the borrowers loan list
+	 * @return the loans.
+	 */
 	public LinkedList<Media> loans() {
 		return loans;
 	}
-
+	/**
+	 * A method that returns the ID of borrower.
+	 * @return Id of a borrower.
+	 */
 	public String getId() {
 		return id;
 	}
-
+	/**
+	 * A method that changes the ID of borrower.
+	 * @param id the id of a specified
+	 * library borrower.
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
-
+	/**
+	 * A method that returns the name of borrower.
+	 * @return a name of a borrower.
+	 */
 	public String getName() {
 		return name;
 	}
-
+	/**
+	 * A method that changes the name of borrower.
+	 * @param name the name of a specified
+	 * library borrower.
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	/**
+	 * A method that returns the phone number of borrower.
+	 * @return a phone number of a borrower.
+	 */
 	public String getPhoneNr() {
 		return phoneNr;
 	}
-
+	/**
+	 * A method that changes the phone number of borrower.
+	 * @param phoneNr the phone nember
+	 *  of a specified library borrower.
+	 */
 	public void setPhoneNr(String phoneNr) {
 		this.phoneNr = phoneNr;
 	}
-	
+	/**
+	 * A toString method that returns all of the available informations
+	 *  a libray borrower.
+	 */
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", phoneNr=" + phoneNr + ", loans=" + loans.toString() + "]";
 	}
--- src/gu1/UserDatabase.java
+++ src/gu1/UserDatabase.java
@@ -2,35 +2,65 @@ package gu1;
 
 import java.util.*;
 import java.util.NoSuchElementException;
-
+/**
+* A class that creats  binary searchable user database
+* @author Jesper Anderberg, Filip Nilsson, Aron Polner, Ali Hassan, Szilveszter Dezsi
+*
+*/
 public class UserDatabase<K, V> implements SearchTree<K, V> {
 	private Comparator<K> comparator;
 	private UserNode<K, V> tree;
 	private int size;
-
+	
+	/**
+	    * Constructs an empty  capacity.
+	    *
+	    * instanstiates compparator
+	    */
 	public UserDatabase() {
 		comparator = new Comp();
 	}
-
+	/**
+	    * Constructor som compares a user tree.
+	    * @param comp compares  users tree
+	    */
 	public UserDatabase(Comparator<K> comp) {
 		comparator = comp;
 	}
-
+	/**
+	 * a method that returns the root of a user tree
+	 * @param root the root of the user tree
+    */
 	public UserNode<K, V> root() {
 		return tree;
 	}
-
+	/**
+    * a method that returns a user  with specified key
+    *in the library borrowers list
+    * @param key  the personal number of the user
+    * @return	the user details such as ID, name and phone number
+    */
 	public V get(K key) {
 		UserNode<K, V> node = find(key);
 		if (node != null)
 			return node.value;
 		return null;
 	}
-
+	/**
+     * puts a borrower  with specified key in the
+     * library user list
+     * @param key  th personal number  of the user 
+     * @param value the user details such as ID, name and phone number 
+     */
 	public void put(K key, V value) {
 		tree = put(tree, key, value);
 	}
-
+	/**
+	    * a method that removes a user  with specified key
+	    *in the library borrowers list
+	    * @param key  the personal number of the user
+	    * @return the user details such as ID, name and phone number
+	    */
 	public V remove(K key) {
 		V value = get(key);
 		if (value != null) {
@@ -39,19 +69,34 @@ public class UserDatabase<K, V> implements SearchTree<K, V> {
 		}
 		return value;
 	}
-
+	/**
+     *a method that  checks  if the user list contains
+     *a user with specified key
+     * @param key  th personal number  of the user
+     */
 	public boolean contains(K key) {
 		return find(key) != null;
 	}
-
+	/**
+     * a method that retunrs the heigth of a user treee
+     *@return the heigth of a tree.
+     */
 	public int height() {
 		return height(tree);
 	}
-
+	/**
+     * a method that creats an iterator object
+     * @return the an iterator object
+     */
 	public Iterator<V> iterator() {
 		return new Iter();
 	}
-
+	/**
+     * a method that finds an a user node  with specified 
+     * personal number and retuns the user object.
+     * @param key  th personal number of the user 
+      @return the user object.
+     */
 	private UserNode<K, V> find(K key) {
 		int res;
 		UserNode<K, V> node = tree;
@@ -63,7 +108,14 @@ public class UserDatabase<K, V> implements SearchTree<K, V> {
 		}
 		return node;
 	}
-
+	/**
+     * puts a borrower  with specified key and value in the
+     * library user list
+     * @param node the user object tu put the list
+     * @param key  th personal number  of the user 
+     * @param value the user details such as ID, name and phone number
+     *  @return the user object.
+     */
 	private UserNode<K, V> put(UserNode<K, V> node, K key, V value) {
 		if (node == null) {
 			node = new UserNode<K, V>(key, value, null, null);
@@ -77,7 +129,14 @@ public class UserDatabase<K, V> implements SearchTree<K, V> {
 		}
 		return node;
 	}
-
+	/**
+     *removes a borrower  with specified key and value in the
+     * library user list
+     * @param node the user object tu put the list
+     * @param key  th personal number  of the user 
+     * @param value the user details such as ID, name and phone number
+     *  @return the user object.
+     */
 	private UserNode<K, V> remove(UserNode<K, V> node, K key) {
 		int compare = comparator.compare(key, node.key);
 		if (compare == 0) {
@@ -100,23 +159,36 @@ public class UserDatabase<K, V> implements SearchTree<K, V> {
 		}
 		return node;
 	}
-
+	/**
+     *  a method that returns the user with the minimum key 
+     * @return the user object.
+     */
 	private UserNode<K, V> getMin(UserNode<K, V> node) {
 		while (node.left != null)
 			node = node.left;
 		return node;
 	}
-
+	/**
+     * a method that retunrs the heigth of a user treee
+     * @param node the the userNode tree 
+     *@return the heigth of a tree.
+     */
 	private int height(UserNode<K, V> node) {
 		if (node == null)
 			return -1;
 		return 1 + Math.max(height(node.left), height(node.right));
 	}
-
+	/**
+	 * A method that returns the size of tree
+	 * @return the size.
+	 */
 	public int size() {
 		return size;
 	}
-	
+	/**
+	 * A method that returns the size of tree
+	 * @return the size.
+	 */
 	public int sizeOfTree() {
 		if (tree != null) {
 			return tree.size();
@@ -124,7 +196,10 @@ public class UserDatabase<K, V> implements SearchTree<K, V> {
 			return 0;
 		}
 	}
-
+	/**
+	 * A method that returns the first user object in the tree of tree
+	 * @return the user details such as ID, name and phone number
+	 */
 	public V first() {
 		UserNode<K, V> firstNode = tree;
 		if (firstNode == null) {
@@ -135,7 +210,10 @@ public class UserDatabase<K, V> implements SearchTree<K, V> {
 		}
 		return firstNode.value;
 	}
-
+	/**
+	 * A method that returns the last user object in the tree of tree
+	 * @return the user details such as ID, name and phone number
+	 */
 	public V last() {
 		UserNode<K, V> lastNode = tree;
 		if (lastNode == null) {
@@ -146,11 +224,18 @@ public class UserDatabase<K, V> implements SearchTree<K, V> {
 		}
 		return lastNode.value;
 	}
-
+	/**
+	 * A method that prints the eements of the user object 
+	 * in the tree of tree
+	 */
 	public void print() {
 		print(tree);
 	}
-
+	/**
+	 * A method that prints the eements of the user object 
+	 * in the tree of tree as inorder 
+	 *@ param node the userBase object to be printed
+	 */
 	private void print(UserNode<K, V> node) {
 		if (node.left != null) {
 			print(node.left);
@@ -160,13 +245,22 @@ public class UserDatabase<K, V> implements SearchTree<K, V> {
 			print(node.right);
 		}
 	}
-
+	/**
+     * a method that lists the keys of the elements
+     * in the user object and rtunrs these keys(personal numbers)
+     *@return the personal numbers of the user objects
+     */
 	public List<K> keys() {
 		ArrayList<K> list = new ArrayList<K>();
 		keys(tree, list);
 		return list;
 	}
-
+	/**
+     * a method that lists the keys of the elements
+     * in the user object 
+     * @param node user object 
+     *@param list of the  user objects
+     */
 	private void keys(UserNode<K, V> node, ArrayList<K> list) {
 		if(node.left!=null){
 			keys(node.left, list);
@@ -176,13 +270,22 @@ public class UserDatabase<K, V> implements SearchTree<K, V> {
 			keys(node.right, list);
 		}
 	}
-
+	/**
+     * a method that lists the values of the elements
+     * in the user object and rtunrs these keys(personal numbers)
+     *@return the personal number, name and phone number of the user. 
+     */
 	public List<V> values() {
 		LinkedList<V> list = new LinkedList<V>();
 		values(tree, list);
 		return list;
 	}
-
+	/**
+     * a method that lists the values of the elements
+     * in the user object 
+     * @param node user object 
+     *@param list of the  user objects
+     */
 	private void values(UserNode<K, V> node, LinkedList<V> list) {
 		if(node.left!=null){
 			values(node.left, list);
@@ -193,22 +296,36 @@ public class UserDatabase<K, V> implements SearchTree<K, V> {
 		}
 		
 	}
-
+	/**
+	 * a local private class that implements
+	 * a comparator and sorts the elements accordin to thier keys
+	 * @return the personal number of a user in the list.
+	 * */
 	private class Comp implements Comparator<K> {
 		public int compare(K key1, K key2) {
 			Comparable<K> k1 = (Comparable<K>) key1;
 			return k1.compareTo(key2);
 		}
 	}
-
+	/**
+	 * A locl class that creates an iterator and implements 
+	 * iterator. 
+	 */
 	private class Iter implements Iterator<V> {
 		ArrayList<V> list = new ArrayList<V>();
 		int index = -1;
-
+		/**
+		 *a metod taht returns a sorts and returns an tree 
+		 *list inOrder. 
+		 */
 		public Iter() {
 			inOrder(tree);
 		}
-
+		/**
+		 *a metod taht returns a sorts and returns an tree 
+		 *list inOrder. 
+		 *@pram a tree object to be sorted inOrder.
+		 */
 		private void inOrder(UserNode<K, V> node) {
 			if (node != null) {
 				inOrder(node.left);
@@ -216,11 +333,19 @@ public class UserDatabase<K, V> implements SearchTree<K, V> {
 				inOrder(node.right);
 			}
 		}
-
+		/**
+		 * a method that returns the last index of an elements in the
+		 * tree . 
+		 * @return last index of a tree
+		 */
 		public boolean hasNext() {
 			return index < list.size() - 1;
 		}
-
+		/**
+		 * a method that returns the  index of an elements in the
+		 * tree . 
+		 * @return  index of a tree
+		 */
 		public V next() {
 			if (!hasNext())
 				throw new NoSuchElementException();
